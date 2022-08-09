6f7111d8edd9433455353b0d7eb89b07febf52a4
Dami Animashaun <theanimashaun@gmail.com>
2022-08-09 13:44:29 +0100

    update useRedditPosts

--- src/hooks/useRedditPosts.js
+++ src/hooks/useRedditPosts.js
@@ -5,6 +5,7 @@ const POSTS_URL =
   'https://api.pushshift.io/reddit/search/submission/?subreddit=reactnative&sort=desc&sort_type=created_utc&size=1000';
 
 const useRedditPosts = () => {
+  const [isLoading, setLoading] = useState(true);
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -12,13 +13,14 @@ const useRedditPosts = () => {
       const data = (await axios.get(POSTS_URL)).data;
       const results = data.data;
       setPosts(results);
+      setLoading(false);
       console.log({a: results.length});
     };
 
     fetchPosts();
   }, []);
 
-  return {posts};
+  return {posts, isLoading};
 };
 
 export {useRedditPosts};
